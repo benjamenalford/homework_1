b9119ac651ef75bfe51ca1482d67408a1332d6bd
Benjamen Alford <benjamen.alford@gmail.com>
2023-08-03 20:55:31 -0500

    psuedo coded work

---
+++ PyPoll.py
@@ -0,0 +1,6 @@
+#open the file
+csv_path = 'PyPoll/Resources/election_data.csv'
+#loop through all votes (rows)
+#count the total of votes
+#save list of canidates
+#number of votes for each candidate
