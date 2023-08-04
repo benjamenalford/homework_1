e2e3d0074b702403e0d5ee482fff5a8b13babd13
Benjamen Alford <benjamen.alford@gmail.com>
2023-08-03 21:01:13 -0500

    created file loop

--- PyPoll.py
+++ PyPoll.py
@@ -1,6 +1,12 @@
-#open the file
+import csv
 csv_path = 'PyPoll/Resources/election_data.csv'
-#loop through all votes (rows)
-#count the total of votes
-#save list of canidates
-#number of votes for each candidate
+#open the file
+with open(csv_path,encoding="UTF-8") as vote_file:
+    #create a csv reader
+    csv_reader = csv.reader(vote_file)
+    #loop through all votes (rows)
+    for vote in csv_reader:
+        #count the total of votes
+        #save list of canidates
+        #number of votes for each candidate
+        print(vote)
