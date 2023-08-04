fc552deade5fbad863f9336ec1a003f927d533ac
Benjamen Alford <benjamen.alford@gmail.com>
2023-08-03 21:40:22 -0500

    almost fin

--- PyPoll.py
+++ PyPoll.py
@@ -4,25 +4,24 @@ csv_path = 'PyPoll/Resources/election_data.csv'
 total_votes =0
 candidates = []
 candidate_votes =[]
+candidates_dict = []
 with open(csv_path,encoding="UTF-8") as vote_file:
     #create a csv reader
     csv_reader = csv.reader(vote_file)
     #get the header
     header = next(csv_reader)
-    print(header)
-
     #loop through all votes (rows)
     for vote in csv_reader:
         #count the total of votes
         total_votes=total_votes+1
         #save list of canidates
         candidate = vote[2]
+        #list logic
         if (candidate not in candidates):
             candidates.append(candidate)
             candidate_votes.append(1)
-            #number of votes for each candidate
-            #print(vote)
         else:
+            #number of votes for each candidate
             current_votes =candidate_votes[candidates.index(candidate)]
             current_votes = current_votes+1
             candidate_votes[candidates.index(candidate)] = current_votes
